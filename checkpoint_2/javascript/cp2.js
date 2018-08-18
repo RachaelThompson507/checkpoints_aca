e4c10b5ff202812d7c42dbe5a0e67941940d7d48
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-08-18 07:42:43 -0500

    javascript file linked

---
+++ checkpoint_2/javascript/cp2.js
@@ -0,0 +1,8 @@
+//javascript for side nav
+function openNav() {
+document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}
+//next section
