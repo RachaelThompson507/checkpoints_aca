82fbeb57d2001bdf9ed974ad0bf08bbf24abb435
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-09-09 19:31:57 -0500

    css init++

---
+++ checkpoint_3/javascript/cp3.js
@@ -0,0 +1,11 @@
+'use strict';
+//*********** DOC READY REQUIRED F(X) *****************
+$(document).ready(function() {
+
+
+
+
+// ********* FOOTER ****************
+  var date = new Date().getFullYear();
+  document.getElementById("copyright").innerHTML = date;
+});
