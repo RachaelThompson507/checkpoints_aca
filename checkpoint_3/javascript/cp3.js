17dad33765f60b747ca4ed025efc90ef6038da4b
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-09-12 18:41:49 -0500

    trying the goCar needs fixing

--- checkpoint_3/javascript/cp3.js
+++ checkpoint_3/javascript/cp3.js
@@ -1,6 +1,41 @@
 'use strict';
 //*********** DOC READY REQUIRED F(X) *****************
 $(document).ready(function() {
+//   var data = {
+//     totalAutoMade:360,
+//     totalCurrent:0,
+//
+//   };
+//   setInterval(goGo,1000);
+//
+//   function goGo() {
+//   data.totalAutoMade+= data.totalRPS;
+//   data.totalCurrent += data.totalRPS;
+//   updateReport();
+// }
+//
+// function updateReport() {
+//   $("#pLs").text(Math.floor(data.totalCurrent));
+// }
+//
+// $(".prodButton").click(function (){
+//   data.totalAutoMade ++;
+//   data.totalCurrent ++;
+//   updateReport();
+// })
+//
+// $(".button").click(function (){
+//   var addVal = $(this).data( "cost" );
+//   if ($(this).data( "cost" ) < data.totalCurrent ) {
+//     data.totalCurrent -=  parseFloat($(this).data( "cost" ).toPrecision(2));
+//     data.totalRPS += parseFloat($(this).data( "val" ));
+//     $( this ).children("span").html( parseInt($( this ).children("span").html()*1.15));
+//     $( this ).data( "cost", parseInt($(this).data( "cost" ) * 1.15) );
+//   }
+//   updateReport();
+// })
+
+
 
   //********* MAKE CARS_Increment Cars REDUCE BANK~DECREMENT**********
   //********* SELL CARS_Decrement Cars INCREASE BANK~INCREMENT **********
