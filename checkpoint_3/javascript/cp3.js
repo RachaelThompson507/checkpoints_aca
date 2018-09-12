e78a07ad413f3174d4f7b2fc1c5a38da70355f95
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-09-11 23:26:14 -0500

    js onClick

--- checkpoint_3/javascript/cp3.js
+++ checkpoint_3/javascript/cp3.js
@@ -2,10 +2,37 @@
 //*********** DOC READY REQUIRED F(X) *****************
 $(document).ready(function() {
 
+  //********* MAKE CARS_Increment Cars REDUCE BANK~DECREMENT**********
+  //********* SELL CARS_Decrement Cars INCREASE BANK~INCREMENT **********
+let numProd = 0;
+let numSold = 0;
+
+
+$('.prodButton').click(function carMade() {
+  numProd += 1;
+  console.log(numProd);
+  $('#pLs').text(`Cars Produced = ${numProd}`)
+  inventoryCall();
+})
+
+$('.sellButton').click(function carSold() {
+  numSold+= 1;
+  console.log(numSold);
+  $('#cSl').text(`Cars Sold = ${numSold}`)
+  inventoryCall();
+})
+
+function inventoryCall(){
+  let inventory = numProd - numSold;
+    $('#inventory').text(`Inventory = ${numProd} - ${numSold} = ${inventory}`);
+}
 
 
 
-// ********* FOOTER ****************
+
+  //********* CREATE BANK and STATS TOTALS**********
+
+  // ********* FOOTER ****************
   var date = new Date().getFullYear();
   document.getElementById("copyright").innerHTML = date;
 });
